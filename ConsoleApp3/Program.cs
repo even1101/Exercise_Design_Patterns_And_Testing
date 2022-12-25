01cf0ea2575bd2ff26e0f6894c52f22d7317225f
even <yesee01.test@yesee.com.tw>
2022-12-25 16:15:15 +0800

    clearn main

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -3,36 +3,9 @@ using ConsoleApp3;
 using ConsoleApp3.State.Light;
 using ConsoleApp3.State.Order;
 
-var light = new Light(new LightOff());
-
-Console.WriteLine(light.Switch());
-Console.WriteLine(light.Switch());
-Console.WriteLine(light.Switch());
-Console.WriteLine(light.Switch());
-Console.WriteLine(light.Switch());
-Console.WriteLine(light.Switch());
-Console.WriteLine(light.Switch());
-Console.WriteLine(light.Switch());
-Console.WriteLine(light.Switch());
 
 
 
-//Console.WriteLine("輸入要產生的類型 C: 圓形,  R:正方形");
-//var type = Console.ReadLine().ToLower();
-
-//if (type == "r")
-//{
-//    Console.WriteLine("R");
-
-//}
-//else if (type == "c")
-//{
-//    Console.WriteLine("C");
-
-//}
-//else {
-//    Console.WriteLine("Bye !");
-//}
 
 
 
